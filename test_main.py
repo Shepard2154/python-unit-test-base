638657fc658f4a9e802ee46b1a6b80fdb5902219
Oleg Burnaev <john-shepard-2154@yandex.ru>
2025-11-19 21:52:26 +0300

    tests: add scenarios

--- test_main.py
+++ test_main.py
@@ -6,7 +6,14 @@ from main import min_of_three_vars
 class MinOfThreeVarsTestCase(unittest.TestCase):
 
     def test_min_a(self):
-        // your test here
+        result = min_of_three_vars(1, 2, 3)
+        self.assertEqual(result, 1)
 
+    def test_min_b(self):
+        result = min_of_three_vars(5, 2, 7)
+        self.assertEqual(result, 2)
 
-        
+    def test_min_c(self):
+        result = min_of_three_vars(10, 15, 8)
+        self.assertEqual(result, 8)
+
